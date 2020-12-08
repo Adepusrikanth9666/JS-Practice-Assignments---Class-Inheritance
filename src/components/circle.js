cfd5a6cbbc437a8b4eede3e0253f1858a5b4ba4e
Adepusrikanth9666 <64652479+Adepusrikanth9666@users.noreply.github.com>
2020-12-08 18:28:14 +0530

    Changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,22 @@
 // import using require
 
+const Shape = require("./shape")
+
 // declare class
+
+class Circle{
+constructor(){
+  this.color = Shape.color;
+
+}
+calculateArea(){}
+
+
+}
+
+let circle = new Circle();
+
 
 
 // export class using module.exports
+module.exports =circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
+class Shape{
+  constructor(shape,Area,property){
+
+this.color = "red";
+
+
+  }
+  drawShape(){}
+  calculateArea(){}
+}
+
+let shape = new Shape();
+
 
 // export class using module.exports
+
+module.exports=shape;
